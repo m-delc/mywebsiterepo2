e7768a151ba60b542f095d5d84fc6a3bb5d5f9b8
MichaelDelcambre <delcambrem@gmail.com>
2021-11-14 15:55:31 -0600

    added index/style/script

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This is pretty fun";
+h2.textContent = "pure magic";
 
 document.querySelector("body").appendChild(h2);
+
+function changeBackground() {
+    var text = document.getElementById("para").style.backgroundColor = "red";
+}
+
+function backToNormal() {
+    var text = document.getElementById("para").style.backgroundColor = "";
+}
+
+function changeBackground() {
+    var text = document.getElementById("para2").style.backgroundColor = "red";
+}
+
+function backToNormal() {
+    var text = document.getElementById("para2").style.backgroundColor = "";
+}
